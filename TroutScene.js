84ec84cb8fc0c20c8f2625ecde6800a104621a24
Property404 <martinez.dagan@gmail.com>
2020-08-22 17:19:42 -0500

    Make a somewhat sensible movables. Upgrade Phaser

--- TroutScene.js
+++ TroutScene.js
@@ -1,11 +1,13 @@
 "use strict";
-const FRICTION = 9000000;
+const FRICTION = 900;
 class TroutScene extends Phaser.Scene
 {
 	player=null;
 	cursors=null;
 	fixtures = [];
 	_sources = new Set();
+	_stuck_movables = new Set();
+
 
 	_preloadFixtures()
 	{
@@ -26,53 +28,88 @@ class TroutScene extends Phaser.Scene
 		for(let i=0;i<this.fixtures.length;i++)
 		{
 			const fixture_data = this.fixtures[i];
-			const group = fixture_data.movable?movables_group:fixture_group;
 			let sprite;
 
 			// Collidables
 			if(!fixture_data.passable)
+			{
+				const group = fixture_data.movable?movables_group:fixture_group;
+
 				sprite = group.create(
 					fixture_data.x,
 					fixture_data.y,
 					fixture_data.src);
-
-			// Passables
+			}
 			else
+			{
+				// Passables
 				sprite = this.add.image(
 					fixture_data.x,
 					fixture_data.y,
 					fixture_data.src);
-
-			sprite.setOrigin(0,0);
+			}
 
 			if(fixture_data.movable)
 			{
-				// Movables just have very high friction
-				sprite.body.setDrag(FRICTION,FRICTION);
+				sprite.body.setDrag(FRICTION);
 			}
 
 			if(fixture_data.scale)
 				sprite.setScale(fixture_data.scale);
 
 			/* TODO: figure out rotation */
+			//sprite.setOrigin(0,0);
+			//updateFromGameObject
 			
 			if(!fixture_data.passable && !fixture_data.movable)
 			{
-				console.log("OH YEAH");
 				sprite.refreshBody();
 			}
 		}
-
-		// TODO:
-		// There's an issue with multiple movables might pass through
-		// a non-passable fixture
-		//
-		// Possible work around: don't allow movables to move
-		// another movable, somehow
 		this.physics.add.collider(this.player, fixture_group);
-		this.physics.add.collider(this.player, movables_group);
-		this.physics.add.collider(movables_group, fixture_group);
-		this.physics.add.collider(movables_group, movables_group);
+
+		// Disallow moving of movables when it hits a static group
+		const stopIt = (a,b)=>{
+			for(const obj of [a,b])
+			{
+				if(obj.body.physicsType != Phaser.Physics.Arcade.STATIC_BODY)
+				{
+					obj.body.immovable = true;
+					this._stuck_movables.add(obj);
+					if(obj === b)
+					{
+						if(obj.body.touching.down)
+						{
+							obj.y--;
+							obj.body.y--;
+						}
+						if(obj.body.touching.up)
+						{
+							obj.y++;
+							obj.body.y++;
+						}
+						if(obj.body.touching.right)
+						{
+							obj.x--;
+							obj.body.x--;
+						}
+						if(obj.body.touching.left)
+						{
+							obj.x++;
+							obj.body.x++;
+						}
+					}
+				}
+			}
+		}
+
+		this.physics.add.collider(movables_group, movables_group, stopIt)
+		this.physics.add.collider(fixture_group, movables_group, stopIt);
+		this.physics.add.collider(this.player, movables_group,
+			(player,obj)=>{
+			}
+		);
+
 	}
 
 	preload()
@@ -116,6 +153,18 @@ class TroutScene extends Phaser.Scene
 			else
 				this.player.setVelocityY(0);
 		}
+
+
+		for(const obj of this._stuck_movables)
+		{
+			if(obj.body.touching.none)
+			{
+				obj.body.immovable=false;
+				this._stuck_movables.delete(obj);
+			}
+		}
+
+
 
 		if(this.updateScene)this.updateScene();
 	}
