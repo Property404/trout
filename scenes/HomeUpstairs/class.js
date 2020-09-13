93171856bd79976eec905b0046bdd0a226c9f0c8
Property404 <martinez.dagan@gmail.com>
2020-09-13 10:35:18 -0500

    Cycle through books on bookshelf instead of showing them randomally. Have door lead out to Bulbasaur World

--- scenes/HomeUpstairs/class.js
+++ scenes/HomeUpstairs/class.js
@@ -8,25 +8,41 @@ class HomeUpstairs extends RoomScene
 			trout.displayDialog(["It's too early to go to bed"])
 		);
 
-		// Look at random book when interacting with the bookshelf
+		// Cycle through books on the bookshelf
 		this.addInteraction("bookshelf", ()=>{
 			const books = [
-				"Alice and Wonderland",
-				"Coraline",
-				"May Bird and the Ever After",
-				"Through the Looking Glass",
+				"The Secret Garden",
+				"Peter Pan",
+				"The BFG",
+				"Gulliver's Travels",
+				"A Wrinkle in Time",
+				"The Little Prince",
 				"The Magician's Nephew",
-				"The Lion, the Witch, and the Wardrobe"
+				"Alice's Adventures in Wonderland",
+				"Bridge to Terabithia",
+				"The Lion, the Witch, and the Wardrobe",
+				"James and the Giant Peach",
+				"The Wonderful Wizard of Oz",
+				"Pippi Longstocking",
+				"May Bird and the Ever After",
+				"May Bird Among the Stars",
+				"May Bird, Warrior Princess",
+				"Coraline",
+				"Alice Through the Looking Glass",
+				"A Treatise of Human Nature",
 			];
-			const book = books[Math.floor(Math.random()*books.length)];
+			this.current_book_index = this.current_book_index || 0;
+			const book = books[this.current_book_index%books.length];
+			this.current_book_index++;
 				
-			trout.displayDialog([book+"...", "...already read it"]);
+			trout.displayDialog([book+"..."]);
 		});
 
 		// This will lead out to the next scene
 		// But seeing as it's not created yet...
+		// ...let's just go to Bulbasaur World
 		this.addInteraction("door", ()=>
-			alert("Next scene not implemented")
+			trout.setPrimaryScene("BulbasaurWorld")
 		);
 	}
 }
